5a147ad5b48e1089245c978d0d560b637a35c6c6
Niño Piña <ninopina10@gmail.com>
2024-12-06 18:36:37 -0600

    Actualizar

---
+++ config/constants.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MAX_SUB_BOTS: 5, // Límite de sub-bots permitidos
+    CODE_EXPIRATION: 60000, // Tiempo de validez del código (1 minuto)
+    CONNECTION_DURATION: 3 * 7 * 24 * 60 * 60 * 1000, // 3 semanas en milisegundos
+};
